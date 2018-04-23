bba4ecd772d120ba795d0d4d3f70db0b0bd0c508
John Deng <john.deng@qq.com>
2018-04-23 16:38:03 +0800

    init commit

---
+++ github.com/hidevopsio/hiboot/README.md
@@ -0,0 +1,3 @@
+# hiboot
+
+'hiboot' is a cloud native web application framework.
---
+++ github.com/hidevopsio/hiboot/pkg/utils/reflect.go
@@ -0,0 +1,12 @@
+package utils
+
+import (
+	"reflect"
+)
+
+func NewReflectType(st interface{}) interface{} {
+	ct := reflect.TypeOf(st)
+	co := reflect.New(ct)
+	cp := co.Elem().Addr().Interface()
+	return cp
+}
